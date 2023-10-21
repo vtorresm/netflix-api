425b1848075a019cf759201ac86080ee485a8ba8
Victor Torres <vatorresm88@gmail.com>
2023-10-21 00:19:43 -0500

    Formateando el codigo

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const cors = require("cors");
-const userRoutes = require("./routes/UserRoutes");
-const mongoose = require("mongoose");
+const express = require('express');
+const cors = require('cors');
+const userRoutes = require('./routes/UserRoutes');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -9,19 +9,19 @@ app.use(cors());
 app.use(express.json());
 
 mongoose
-  .connect("mongodb://localhost:27017/netflix", {
+  .connect('mongodb://localhost:27017/netflix', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
-    console.log("DB Connetion Successfull");
+    console.log('DB Connetion Successfull');
   })
   .catch((err) => {
     console.log(err.message);
   });
 
-app.use("/api/user", userRoutes);
+app.use('/api/user', userRoutes);
 
 app.listen(5000, () => {
-  console.log("server started on port 5000");
+  console.log('server started on port 5000');
 });
